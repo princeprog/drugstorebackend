b20970433ef80360017e39f51eabe371d212db65
Al Prince Llavan <alprincellavan2019@gmail.com>
2025-01-28 20:05:51 +0800

    initial commit

---
+++ src/test/java/com/pach/drugstore/DrugstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pach.drugstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
