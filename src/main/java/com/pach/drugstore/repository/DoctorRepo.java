875afd1631357de356710f09cdddcee9b8badb51
Felix Castañeda <felixjoseph.castaneda@cit.edu>
2025-01-29 14:16:59 +0800

    Doctor Entity (REMOVED)

--- src/main/java/com/pach/drugstore/repository/DoctorRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pach.drugstore.repository;
-
-import com.pach.drugstore.entity.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DoctorRepo extends JpaRepository<Doctor, Integer> {
-}
